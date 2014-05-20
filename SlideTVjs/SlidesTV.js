be698b343df138121b76a1cdcb5a3608b9ed44d0
suraphol laotarkunchai <suraphol@clouddha.com>
2014-05-20 14:27:05 +0700

    new file coding

---
+++ SlideTVjs/SlidesTV.js
@@ -0,0 +1,127 @@
+// setup ****
+
+SlidesTV = {
+  type         : 1,
+  pagination   : true,
+
+  index        : 1,
+  left         : 0,
+  top          : 0,
+  width        : 600,
+  height       : 200,
+  size         : 0,
+
+  dulation     : 500,
+};
+
+SlidesTV.Initial = function() {
+  this.size = $("#SlidesTV div").length;
+
+
+  var HTML_TEMP = '';
+
+  HTML_TEMP += '<div id="SlidesTV-container">';
+  HTML_TEMP += '  <div id="SlidesTV-control">';
+  HTML_TEMP += '  </div>';
+  HTML_TEMP += '</div>';
+
+  $("#SlidesTV").append(HTML_TEMP);
+
+  $("#SlidesTV").css({"position":"relative","width":SlidesTV.width+"px","height":SlidesTV.height+"px"});
+  $("#SlidesTV-container").css({"position":"relative","width":SlidesTV.width+"px","height":SlidesTV.height+"px"});
+
+  SlidesTV.AddContent();
+  if (SlidesTV.pagination) {
+    SlidesTV.CreatePagination();
+  }
+  SlidesTV.Select();
+
+};
+
+SlidesTV.AddContent = function() {
+  for (var i=0; i<this.size; i++) {
+    var SlideItem = $("#SlidesTV div").eq(0);
+    SlideItem.attr("class","Slide-item-"+i);
+    $("#SlidesTV-control").append(SlideItem);
+    SlideItem.css({"position":"absolute","display":"block","left":(i*SlidesTV.width)+"px","top":"0px","width":"100%","height":"100%","opacity":"0.7"});
+  }
+  $("#SlidesTV-control").css({"left":"-"+(SlidesTV.width*SlidesTV.index)+"px"});
+};
+
+SlidesTV.CreatePagination = function() {
+
+  var HTML_TEMP = '';
+  HTML_TEMP += '<ul id="SlidesTV-pagination">';
+    for (var i=0; i<this.size; i++) {
+      HTML_TEMP += '  <li class="pagintion-item-'+i+'">';
+      HTML_TEMP += '    <div class="pagination"></div>';
+      HTML_TEMP += '  </li>';
+    }
+  HTML_TEMP += '</ul>';
+
+  $("#SlidesTV").append(HTML_TEMP);
+};
+
+SlidesTV.Next = function() {
+  switch (SlidesTV.type) {
+  case 1:
+    var range = SlidesTV.size*SlidesTV.width;
+    if (SlidesTV.index == SlidesTV.size-1) {
+      SlidesTV.index = 0;
+    }else {
+      SlidesTV.index++;
+    }
+
+    $("#SlidesTV-control").animate({"left":"-="+SlidesTV.width+"px"},SlidesTV.dulation);
+    SlidesTV.Select();
+    setTimeout(function () {
+      var SlideItem = $("#SlidesTV-control div").eq(0);
+      SlideItem.css("left","+="+range+"px");
+      $("#SlidesTV-control").append(SlideItem);
+
+    }, SlidesTV.dulation);
+    break;
+  default: // default is slide normal end
+    if (SlidesTV.index < SlidesTV.size-1) {
+      SlidesTV.index++;
+      $("#SlidesTV-control").animate({"left":"-="+SlidesTV.width+"px"},SlidesTV.dulation);
+      SlidesTV.Select();
+    }
+  }
+};
+
+SlidesTV.Back = function() {
+  switch (SlidesTV.type) {
+  case 1:
+    var range = SlidesTV.size*SlidesTV.width;
+
+    var SlideItem = $("#SlidesTV-control div").eq(SlidesTV.size-1);
+    SlideItem.css("left","-="+range+"px");
+    $("#SlidesTV-control").prepend(SlideItem);
+
+    if (SlidesTV.index == 0) {
+      SlidesTV.index = SlidesTV.size-1;
+    }else {
+      SlidesTV.index--;
+    }
+
+    $("#SlidesTV-control").animate({"left":"+="+SlidesTV.width+"px"},SlidesTV.dulation);
+    SlidesTV.Select();
+    break;
+  default: // default is slide normal end
+    if (SlidesTV.index > 0) {
+      SlidesTV.index--;
+      $("#SlidesTV-control").animate({"left":"+="+SlidesTV.width+"px"},SlidesTV.dulation);
+      SlidesTV.Select();
+    }
+  }
+};
+
+SlidesTV.Select = function() {
+  for (var i=0; i<this.size; i++) {
+    $("#SlidesTV-control .Slide-item-"+i).animate({"opacity":"0.7"});
+    $("#SlidesTV-pagination li.pagintion-item-"+i+" div").removeClass("pagination-active");
+  }
+  $("#SlidesTV-control .Slide-item-"+SlidesTV.index).animate({"opacity":"1.0"},SlidesTV.dulation);
+  $("#SlidesTV-pagination li.pagintion-item-"+SlidesTV.index+" div").addClass("pagination-active");
+}
---
+++ slideHTML.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>SlideHTML</title>
+
+    <script src="./SlideTVjs/SlidesTV.js" ></script>
+
+    <link rel="stylesheet" href="./SlideTVcss/SlidesTV.css">
+  </head>
+  <body>
+
+    <div id="#SlidesTV">
+      <?php
+        for (int i=0; i<4; i++) {
+          
+        }
+      ?>
+    </div>
+
+    test
+
+  </body>
+</html>
